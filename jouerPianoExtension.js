945a7e95dd931fb04486ef2772c596e9155a3c20
Sylvain <chambon.sylvain@gmail.com>
2022-12-14 10:37:43 +0100

    fix: enlever superflu

--- jouerPianoExtension.js
+++ jouerPianoExtension.js
@@ -9,60 +9,63 @@ const menuIconURI = null;
 const blockIconURI = null;
 
 class jouerPianoExt{
-  constructor (runtime){
-    this.runtime = runtime;
-    // communication related
-    this.comm = runtime.ioDevices.comm;
-    this.session = null;
-    this.runtime.registerPeripheralExtension('jouerPianoExt', this);
-    // session callbacks
-    this.reporter = null;
-    this.onmessage = this.onmessage.bind(this);
-    this.onclose = this.onclose.bind(this);
-    this.write = this.write.bind(this);
-    // string op
-    this.decoder = new TextDecoder();
-    this.lineBuffer = '';
-  }
 
-  onclose (){
-    this.session = null;
+  constructor() {
   }
+  // constructor (runtime){
+  //   this.runtime = runtime;
+  //   // communication related
+  //   this.comm = runtime.ioDevices.comm;
+  //   this.session = null;
+  //   this.runtime.registerPeripheralExtension('jouerPianoExt', this);
+  //   // session callbacks
+  //   this.reporter = null;
+  //   this.onmessage = this.onmessage.bind(this);
+  //   this.onclose = this.onclose.bind(this);
+  //   this.write = this.write.bind(this);
+  //   // string op
+  //   this.decoder = new TextDecoder();
+  //   this.lineBuffer = '';
+  // }
 
-  write (data, parser = null){
-    if (this.session){
-      return new Promise(resolve => {
-        if (parser){
-          this.reporter = {
-            parser,
-            resolve
-          }
-        }
-        this.session.write(data);
-      })
-    }
-  }
+  // onclose (){
+  //   this.session = null;
+  // }
 
-  onmessage (data){
-    const dataStr = this.decoder.decode(data);
-    this.lineBuffer += dataStr;
-    if (this.lineBuffer.indexOf('\n') !== -1){
-      const lines = this.lineBuffer.split('\n');
-      this.lineBuffer = lines.pop();
-      for (const l of lines){
-        if (this.reporter){
-          const {parser, resolve} = this.reporter;
-          resolve(parser(l));
-        };
-      }
-    }
-  }
+  // write (data, parser = null){
+  //   if (this.session){
+  //     return new Promise(resolve => {
+  //       if (parser){
+  //         this.reporter = {
+  //           parser,
+  //           resolve
+  //         }
+  //       }
+  //       this.session.write(data);
+  //     })
+  //   }
+  // }
 
-  scan (){
-    this.comm.getDeviceList().then(result => {
-        this.runtime.emit(this.runtime.constructor.PERIPHERAL_LIST_UPDATE, result);
-    });
-  }
+  // onmessage (data){
+  //   const dataStr = this.decoder.decode(data);
+  //   this.lineBuffer += dataStr;
+  //   if (this.lineBuffer.indexOf('\n') !== -1){
+  //     const lines = this.lineBuffer.split('\n');
+  //     this.lineBuffer = lines.pop();
+  //     for (const l of lines){
+  //       if (this.reporter){
+  //         const {parser, resolve} = this.reporter;
+  //         resolve(parser(l));
+  //       };
+  //     }
+  //   }
+  // }
+
+  // scan (){
+  //   this.comm.getDeviceList().then(result => {
+  //       this.runtime.emit(this.runtime.constructor.PERIPHERAL_LIST_UPDATE, result);
+  //   });
+  // }
 
   getInfo (){
     return {
